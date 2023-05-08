ca1f5b110e7e66ecb4e0d1256e4bf6cc5834cc23
Collect_diamond <3066703409@qq.com>
2023-05-08 14:28:29 +0800

    1

--- api/testproxy.js
+++ api/testproxy.js
@@ -1,22 +1,35 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const requestIp = require("request-ip");
 
-module.exports = (req, res) => {
-	let target = "";
+module.exports = async (req, res) => {
+	try {
+		const api_key = "69eea6af430b21f802683f2d9b198ba8";
+		// 获取用户IP地址
+		const ip = await requestIp.getClientIp(req);
 
-	// 代理目标地址
-	// 这里使用 backend-ip 主要用于区分 vercel serverless 的 api 路径
-	if (req.url.startsWith("/backend")) {
-		target = "https://ipapi.co";
+		if (!ip) {
+			// 没有获取到用户IP地址，返回错误信息
+			return res.status(400).send("Unable to get user IP address");
+		}
+
+		const target = `http://api.ipapi.com/api/${ip}
+    ? access_key = ${api_key}`;
+
+		// 创建代理对象并转发请求
+		createProxyMiddleware({
+			target,
+			changeOrigin: true,
+			secure: false,
+			headers: {
+				Connection: "keep-alive",
+			},
+			pathRewrite: {
+				"^/backend/": "/",
+			},
+		})(req, res);
+	} catch (error) {
+		// 出错处理逻辑
+		console.error(error);
+		res.status(500).send("Internal Server Error");
 	}
-
-	// 创建代理对象并转发请求
-	createProxyMiddleware({
-		target,
-		changeOrigin: true,
-		pathRewrite: {
-			// 通过路径重写，去除请求路径中的 `/backend`
-			// 例如 /backend/user/login 将被转发到 https://ipapi.co
-			"^/backend/": "/",
-		},
-	})(req, res);
 };
